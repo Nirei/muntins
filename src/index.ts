23bc853faba318e5b5fd2fdfc94f5cea43d68f30
Nirei <nireiorange@gmx.com>
2026-03-16 00:26:19 +0100

    feat(ui): add Button component

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@
 export * from "./core/index.ts";
 
 // UI Components
+export {
+  Button,
+  type ButtonProps,
+  type ButtonVariant,
+} from "./ui/button.ts";
 export { Label, type LabelProps } from "./ui/label.ts";
 export {
   Separator,
